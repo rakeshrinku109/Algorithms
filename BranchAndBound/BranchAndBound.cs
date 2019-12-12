f7447ac229fecc135a825e2d0b37f3b38cf9adff
RakeshPadhi <rakesh.rinku109@gmail.com>
2019-12-12 10:41:16 +0530

    Branch and Bound  - Agent and Task combination.
    N tasks to be distributed among M agents, with each combination assigned a deadline or cost. Find the minimum cost where every agent does some task,

---
+++ BranchAndBound/BranchAndBound.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using C5;
+
+namespace Module4Examples
+{
+    class BranchAndBound
+    {
+        int[,] costMatrix;
+        int numTasks;
+        int numAgents;
+        int[] minimumCosts;
+
+        class SolutionCandidate : IComparable, ICloneable
+        {
+            public int LowerBound;
+            public List<int> Assignments;
+
+            public int CompareTo(object obj)
+            {
+                return LowerBound.CompareTo(((SolutionCandidate)obj).LowerBound);
+            }
+
+            public object Clone()
+            {
+                var clone = new SolutionCandidate
+                {
+                    LowerBound = this.LowerBound,
+                    Assignments = new List<int>(this.Assignments)
+                };
+                return clone;
+            }
+        }
+
+        public BranchAndBound(int[,] costMatrix)
+        {
+            this.costMatrix = costMatrix;
+            numAgents = costMatrix.GetLength(0);
+            numTasks = numAgents; // For this problem, these two are always equal.
+
+            // Find minimum costs for each column:
+            minimumCosts = (int[])Array.CreateInstance(typeof(int), numAgents);
+            for (var agent = 0; agent < numAgents; agent++)
+            {
+                minimumCosts[agent] = costMatrix[agent, 0];
+                for (var task = 1; task < numTasks; task++)
+                    minimumCosts[agent] = Math.Min(minimumCosts[agent], costMatrix[agent, task]);
+            }
+        }
+
+        public List<int> FindAssignment(out int solutionCost)
+        {
+            // Initialize first solution candidate:
+            var currentSolution = new SolutionCandidate() { Assignments = new List<int>() };
+            for (var i = 0; i < numAgents; i++)
+            {
+                currentSolution.Assignments.Add(i);
+                currentSolution.LowerBound += costMatrix[i, i];
+            }
+
+            // Prepare queue and initialize it with an empty candidate:
+            var queue = new IntervalHeap<SolutionCandidate>(); // A queue or a stack could be used instead.
+            queue.Add(new SolutionCandidate() { Assignments = new List<int>() });
+
+            // Here is the main algorithm...
+            while (!queue.IsEmpty)
+            {
+                // Get next candidate. If a priority queue is used, the "next" candidate is the
+                // most promising (with lowest lower bound):
+                var candidate = queue.DeleteMin();
+
+                // Branch out:
+                for (var task = 0; task < numTasks; task++)
+                {
+                    // Only create the branch if the current task is not already taken by an agent:
+                    if (!candidate.Assignments.Contains(task)) // A HashSet on the side could speed up the .Contains() call...
+                    {
+                        var branch = (SolutionCandidate)candidate.Clone();
+                        branch.Assignments.Add(task);
+                        branch.LowerBound = CalculateLowerBound(branch);
+
+                        // Only use the generated branch if its lower bound is strictly better than
+                        // the currently found solution cost.
+                        // This is the "bound" part in "Branch and Bound":
+                        if (branch.LowerBound >= currentSolution.LowerBound)
+                            continue;
+
+                        // Do we have a solution?
+                        if (branch.Assignments.Count == numAgents)
+                            currentSolution = branch;
+                        else // If not, use it for further extension:
+                            queue.Add(branch);
+                    }
+                }
+            }
+            solutionCost = currentSolution.LowerBound;
+            return currentSolution.Assignments;
+        }
+
+        int CalculateLowerBound(SolutionCandidate branch)
+        {
+            var lowerBound = 0;
+            // Add the costs for the already fixed agents:
+            for (var agent = 0; agent < branch.Assignments.Count; agent++)
+            {
+                var task = branch.Assignments[agent];
+                lowerBound += costMatrix[agent, task];
+            }
+
+            // Add "minimum cost" for remaining agents:
+            for (var agent = branch.Assignments.Count; agent < numAgents; agent++)
+                lowerBound += minimumCosts[agent];
+
+            return lowerBound;
+        }
+    }
+}
---
+++ BranchAndBound/Program.cs
@@ -0,0 +1,161 @@
+﻿using C5;
+using Module4Examples;
+using System;
+using System.Collections.Generic;
+
+namespace DivideAndConquer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        static void RunBranchAndBoundAssignment()
+        {
+            var costMatrix = new int[4, 4]
+            {
+                { 3, 5, 9, 2},
+                { 9, 3, 3, 4},
+                { 1, 4, 2, 6},
+                { 5, 3, 7, 2}
+            };
+
+            var solver = new BranchAndBound(costMatrix);
+            var solutionCost = 0;
+            var assignment = solver.FindAssignment(out solutionCost);
+
+            Console.WriteLine("Assignment cost: {0}", solutionCost);
+            for (var agent = 0; agent < assignment.Count; agent++)
+                Console.WriteLine("   Agent {0}: Task {1}", agent, assignment[agent]);
+        }
+
+    }
+
+    public class BnB
+    {
+        int[,] CostMatrix;
+        int NumberOfAgents;
+        int NumberOFTasks;
+        int[] MinimumCosts;
+        IntervalHeap<SolutionCandidate> SolutionCandidatePriorityQueue = new IntervalHeap<SolutionCandidate>();
+
+        public BnB(int[,] costMatrix, int m, int n)
+        {
+            this.CostMatrix = costMatrix;
+            this.NumberOFTasks = n;
+            this.NumberOfAgents = m;
+            this.MinimumCosts = new int[NumberOfAgents];
+        }
+
+        public void EvaluateCost()
+        {
+            BuildMinimumCostTable(NumberOfAgents,NumberOFTasks);
+
+            SolutionCandidate currentSolution = new SolutionCandidate();
+            currentSolution.Assignments = new System.Collections.Generic.List<int>();
+
+            for (int i = 0; i < NumberOfAgents; i++)
+            {
+                currentSolution.Assignments.Add(i);
+                currentSolution.LowerBound += this.CostMatrix[NumberOfAgents, NumberOfAgents];
+            }
+
+            SolutionCandidatePriorityQueue.Add(new SolutionCandidate() { Assignments = new System.Collections.Generic.List<int>() });
+
+            while (!SolutionCandidatePriorityQueue.IsEmpty)
+            {
+                // Get next candidate. If a priority queue is used, the "next" candidate is the
+                // most promising (with lowest lower bound):
+                var candidate = SolutionCandidatePriorityQueue.DeleteMin();
+
+                // Branch out:
+                for (int i = 0; i < NumberOFTasks; i++)
+                {
+                    // Only create the branch if the current task is not already taken by an agent:
+                    if (!candidate.Assignments.Contains(i))
+                    {
+                        var branch = (SolutionCandidate)candidate.Clone();
+                        branch.Assignments.Add(i);
+                        branch.LowerBound = CalculateLowerBound(branch);
+
+                        // Only use the generated branch if its lower bound is strictly better than
+                        // the currently found solution cost.
+                        // This is the "bound" part in "Branch and Bound":
+                        if (currentSolution.LowerBound < branch.LowerBound)
+                            continue;
+
+                        if (branch.Assignments.Count == NumberOfAgents)
+                        {
+                            currentSolution = branch;
+                        }
+                        else
+                        {
+                            this.SolutionCandidatePriorityQueue.Add(branch);
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+        private int CalculateLowerBound(SolutionCandidate candidate)
+        {
+            int lowerBound = 0;
+            int Agent = 0;
+            // Add the costs for the already fixed agents:
+            for (; Agent < candidate.Assignments.Count; Agent++)
+            {
+                var task = candidate.Assignments[Agent];
+                lowerBound += this.CostMatrix[Agent, task];
+            }
+
+            for (int j = Agent; j < MinimumCosts.Length; j++)
+            {
+                lowerBound += MinimumCosts[j];
+            }
+
+            return lowerBound;
+        }
+
+        private void BuildMinimumCostTable(int row, int column)
+        {
+            while(row >= 0)
+            {
+                int min = this.CostMatrix[row, 0];
+                for (int i = 1; i < column; i++)
+                {
+                   min = Math.Min(min, this.CostMatrix[row, i]);
+                }
+                this.MinimumCosts[row] = min;
+            }
+        }
+    }
+
+    public class SolutionCandidate : IComparable
+    {
+        public int LowerBound = 0;
+        public List<int> Assignments { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return this.LowerBound.CompareTo(((SolutionCandidate)obj).LowerBound);
+        }
+
+        public SolutionCandidate Clone()
+        {
+            SolutionCandidate Clone = new SolutionCandidate()
+            {
+                LowerBound = this.LowerBound,
+                Assignments = this.Assignments
+            };
+            
+            return Clone;
+        }
+    }
+
+
+}
